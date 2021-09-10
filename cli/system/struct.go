2224d398822b4511eaa1a64c787bc38ee4deabc7
Edgar Javier <akosej9208@gmail.com>
2021-09-10 17:43:20 -0400

    update struct

--- cli/system/struct.go
+++ cli/system/struct.go
@@ -5,6 +5,5 @@ type Model struct {
 	Quota    float64 `json:"quota" redis:"quota" binding:"required"`
 	Used     float64 `json:"used" redis:"used"`
 	Update   string  `json:"update" redis:"update"`
-	IpRemote string  `json:"ipremote" redis:"ipremote"`
 	Bloquear bool    `json:"bloquear" redis:"bloquear"`
 }
